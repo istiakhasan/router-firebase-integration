672d93c37fd8b1fe04cb39a255077eb57dfaf461
Istieak <85983888+Istieak@users.noreply.github.com>
2022-04-08 22:44:21 +0600

    initial commit

---
+++ src/firebase.init.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCE8Ah6adr-AjwLf4BnbDip9jmvWO0BVoo",
+  authDomain: "router-firebase-integrat-1e392.firebaseapp.com",
+  projectId: "router-firebase-integrat-1e392",
+  storageBucket: "router-firebase-integrat-1e392.appspot.com",
+  messagingSenderId: "648722302570",
+  appId: "1:648722302570:web:5714e69892383cbdf36c13"
+};
+
+const app = initializeApp(firebaseConfig);
+export default app;
